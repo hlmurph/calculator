995a3f1b9c3225a2f76cd2594379567f14ca9a8b
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-04-14 16:05:26 -0400

    Create functions for operations and function to delegate those operations

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # calculator
+
+Pseudo code
+
+<!-- Create functions for operators
+    Add
+    Subrtact
+    Multiply
+    Divide -->
+
+<!-- Create variables to update display -->
+
+<!-- Create operate function that takes an operator and two numbers, then calls the operator function -->
+
+Create HTML calculator
+    Number pad
+    Display
+    Clear button
+
+Create functions to populate display when buttons are clicked and store in a variable
+
+Store first and second numbers in variables and call operator
+Update display with result
---
+++ script.js
@@ -0,0 +1,32 @@
+let input1;
+let input2;
+let operation;
+
+function add(a, b) {
+    return a + b;  
+}
+function sub(a, b) {
+    return a - b;
+}
+function mult(a, b) {
+    return a * b;
+}
+function div(a, b) {
+    return a / b;
+}
+
+function operate(operation, a, b) {
+    switch (operation) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return sub(a, b);
+            break;
+        case '*':
+            return mult(a, b);
+            break;
+        case '/':
+            return div(a, b);
+    }
+}
